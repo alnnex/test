133d0fdf29de90fe4624548bcca9356fc3d750da
alnnex <alnnexquiwag123@gmail.com>
2022-10-12 20:27:41 +0800

    first

---
+++ public/crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "4704d5cc-a57e-4213-b785-f20bd9fb1646";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
