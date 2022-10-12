8c1ce18b92adeb142b470f514ed7ff561f355b62
alnnex <alnnexquiwag123@gmail.com>
2022-10-12 20:48:02 +0800

    content

---
+++ components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <h1>I love you LALABS.. hehehehe</h1>
+  )
+}
+
+export default Content
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Script from "next/script";
 import Header from "../components/Header";
+import Content from "../components/Content";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Content />
       <Script src="/crisp.js" />
     </>
   );
