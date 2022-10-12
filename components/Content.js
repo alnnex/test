992593b258402293a1a418adff86ba0751cd09b6
alnnex <alnnexquiwag123@gmail.com>
2022-10-12 21:38:51 +0800

    modal edit

--- components/Content.js
+++ components/Content.js
@@ -1,44 +1,48 @@
 import React from "react";
+import Image from "next/image";
 
 function Content() {
   return (
-    <>
+    <div className="d-flex justify-content-center">
       <button
         type="button"
-        class="btn btn-primary"
+        className="btn btn-primary"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
       >
-        Launch demo modal
+        Click to toggle Modal Component
       </button>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="exampleModal"
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Modal title
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modal Test Success
               </h1>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">
-              <h1>I love you LALABS.. hehehehe</h1>
+            <div className="modal-body">
+              <h1>BootStrap Test Success if Modal Appears</h1>
+              <div className="d-flex justify-content-center">
+                <img className="h-50 w-50" src="https://imageio.forbes.com/blogs-images/rainerzitelmann/files/2019/05/GettyImages-481406443-1200x1785.jpg?format=jpg&width=960" />
+              </div>
             </div>
-            <div class="modal-footer">
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
+                className="btn btn-secondary"
                 data-bs-dismiss="modal"
               >
                 Close
@@ -47,7 +51,7 @@ function Content() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
